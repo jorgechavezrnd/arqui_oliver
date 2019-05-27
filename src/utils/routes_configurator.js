7c1cebdaf6c803dd902de23156ac2d45994256af
jorgechavezrnd <jorgechavezrnd@gmail.com>
2019-05-27 04:01:57 +0000

    view model creado para ejs

--- src/utils/routes_configurator.js
+++ src/utils/routes_configurator.js
@@ -3,6 +3,9 @@ const path = require('path');
 const APIRESTController = require(
     path.join(process.cwd(), 'src', 'presentation', 'apiRest', 'controller', 'api_rest_controller')
 );
+const EmployeeRegistrationFormViewModel = require(
+    path.join(process.cwd(), 'src', 'presentation', 'ejs', 'viewModel', 'employee_registration_form_view_model')
+);
 
 const router = new Router();
 
@@ -16,7 +19,21 @@ router.post('/registerEmployeeUseCase', async (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    res.render('employee_registration_form_view');
+    let employeeRegistrationFormViewModel = new EmployeeRegistrationFormViewModel(
+        'Nuevo Empleado',
+        'CI',
+        'Nombre',
+        'Tipo',
+        'Está en el sidicato',
+        'Registrar',
+        {
+            'Por hora': 'H',
+            'Fijo': 'F',
+            'Por comisión': 'C',
+        }
+    );
+
+    res.render('employee_registration_form_view', { model: employeeRegistrationFormViewModel });
 });
 
 module.exports = router;
